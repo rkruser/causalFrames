19eee61d6192d7f68bb90d7c945a3a24b88debca
Ryen Krusinga <krusinga@cs.umd.edu>
2019-07-12 13:22:03 -0400

    Decent version of new_proc_vid

--- new_proc_vid.py
+++ new_proc_vid.py
@@ -33,15 +33,167 @@ from PIL import Image
 
 
 datapath = '/scratch1/datasets/beamng/extracted_vids'
-vidnames = [
+# vidnames = [
+#     'v1_1.mp4',
+#     'v1_2.mp4',
+#     'v1_3.mp4',
+#     'v16_1.mp4'
+# ]
+
+trainvids = [
     'v1_1.mp4',
-    'v1_2.mp4'
+    'v1_2.mp4',
+    'v1_3.mp4',
+    'v1_4.mp4',
+    'v1_5.mp4',
+    'v1_6.mp4',
+    'v1_7.mp4',
+    'v1_8.mp4',
+    'v1_9.mp4',
+    'v1_10.mp4',
+    'v1_11.mp4',
+    'v1_12.mp4',
+    'v1_13.mp4',
+    'v1_14.mp4',
+    'v2_2.mp4',
+    'v2_4.mp4',
+    'v2_7.mp4',
+    'v2_9.mp4',
+    'v2_11.mp4',
+    'v3_1.mp4',
+    'v3_2.mp4',
+    'v3_3.mp4',
+    'v3_4.mp4',
+    'v3_5.mp4',
+    'v3_7.mp4',
+    'v4_1.mp4',
+    'v4_3.mp4',
+    'v4_4.mp4',
+    'v4_5.mp4',
+    'v4_6.mp4',
+    'v4_8.mp4',
+    'v4_9.mp4',
+    'v4_10.mp4',
+    'v4_13.mp4',
+    'v4_14.mp4',
+    'v4_15.mp4',
+    'v4_17.mp4',
+    'v4_18.mp4',
+    'v5_1.mp4',
+    'v5_2.mp4',
+    'v5_3.mp4',
+    'v5_4.mp4',
+    'v5_5.mp4',
+    'v5_6.mp4',
+    'v5_7.mp4',
+    'v5_8.mp4',
+    'v6_1.mp4',
+    'v6_2.mp4',
+    'v6_3.mp4',
+    'v6_4.mp4',
+    'v6_5.mp4',
+    'v7_1.mp4',
+    'v7_2.mp4',
+    'v7_3.mp4',
+    'v7_4.mp4',
+    'v7_5.mp4',
+    'v7_6.mp4',
+    'v7_8.mp4',
+    'v7_9.mp4',
+    'v7_11.mp4',
+    'v7_14.mp4',
+    'v7_15.mp4',
+    'v7_17.mp4',
+    'v7_18.mp4',
+    'v8_1.mp4',
+    'v8_2.mp4',
+    'v8_3.mp4',
+    'v8_4.mp4',
+    'v8_5.mp4',
+    'v8_6.mp4',
+    'v9_1.mp4',
+    'v9_3.mp4',
+    'v9_4.mp4',
+    'v10_2.mp4',
+    'v10_3.mp4',
+    'v10_4.mp4',
+    'v11_1.mp4',
+    'v11_2.mp4',
+    'v11_3.mp4',
+    'v11_4.mp4',
+    'v11_5.mp4',
+    'v11_6.mp4',
+    'v12_1.mp4',
+    'v13_1.mp4',
+    'v13_2.mp4',
+    'v13_3.mp4',
+    'v13_4.mp4',
+    'v13_5.mp4',
+    'v13_6.mp4',
+    'v13_7.mp4',
+    'v14_1.mp4',
+    'v14_2.mp4',
+    'v14_3.mp4',
+    'v14_4.mp4',
+    'v14_6.mp4',
+    'v14_7.mp4',
+    'v15_1.mp4',
+    'v15_2.mp4',
+    'v15_3.mp4',
+    'v15_4.mp4',
+    'v15_5.mp4',
+    'v15_7.mp4',
+    'v15_8.mp4',
+    'v15_9.mp4',
+    'v15_11.mp4',
+    'v15_12.mp4',
+    'v15_13.mp4',
+    'v16_1.mp4',
+    'v16_2.mp4',
+    'v16_3.mp4',
+    'v16_4.mp4',
+    'v16_6.mp4',
+    'v16_7.mp4',
+    'v16_9.mp4',
+    'v16_10.mp4'
+]
+
+testvids = [
+    'v12_2.mp4',
+    'v4_12.mp4',
+    'v15_10.mp4',
+    'v7_16.mp4',
+    'v10_1.mp4',
+    'v4_19.mp4',
+    'v4_7.mp4',
+    'v2_6.mp4',
+    'v13_8.mp4',
+    'v3_6.mp4',
+    'v4_16.mp4',
+    'v7_13.mp4',
+    'v7_10.mp4',
+    'v5_9.mp4',
+    'v4_11.mp4',
+    'v7_7.mp4',
+    'v9_2.mp4',
+    'v6_6.mp4',
+    'v2_5.mp4',
+    'v16_5.mp4',
+    'v7_12.mp4',
+    'v4_2.mp4',
+    'v2_3.mp4',
+    'v14_5.mp4',
+    'v2_1.mp4',
+    'v16_8.mp4',
+    'v15_6.mp4',
+    'v2_8.mp4',
+    'v2_10.mp4'
 ]
 
 
 class VideoDataset(Dataset):
   def __init__(self, path=datapath, 
-        names=vidnames, 
+        names=trainvids, 
         labelfile='times.txt',
         keepEvery=5,
         cropsize=768,
@@ -193,13 +345,13 @@ class VideoDataset(Dataset):
 
     return returnFrames, returnLabel
 
-    def numVideos(self):
-        return len(self.cumulFrames)-1
+  def numVideos(self):
+    return len(self.cumulFrames)-1
 
-    def getVideo(self, number):
-        startindex = self.cumulFrames[number]
-        endindex = self.cumulFrames[number+1]
-        return self.allframes[startindex:endindex]
+  def getVideo(self, number):
+    startindex = self.cumulFrames[number]
+    endindex = self.cumulFrames[number+1]
+    return self.allframes[startindex:endindex]
 
 
 
@@ -207,18 +359,41 @@ class VideoDataset(Dataset):
 
 
 if __name__ == '__main__':
-    d = VideoDataset(load_from_tensorfile=False, windowsize=1, 
-        save_to_tensorfile=False)
-    print(len(d))
-    out0, out1 = d[96]
-    out0 = out0.numpy().transpose(1,2,0)
+    traindata = VideoDataset(load_from_tensorfile=True, 
+        windowsize=2, 
+        save_to_tensorfile=True, 
+        tensorfile_prefix='allframes_224_train',
+        names=trainvids)
+    print(len(traindata))
+    testdata = VideoDataset(load_from_tensorfile=True,
+        windowsize=2,
+        save_to_tensorfile=True,
+        tensorfile_prefix='allframes_224_test',
+        names=testvids
+        )
+    print(len(testdata))
+    # print(d.numVideos())
+    # vid2 = d.getVideo(2)
+    # print(vid2.size())
+    # print(d.metadata)
+    # print(len(d))
+    # print(len(d))
+    # out0, out1 = d[96]
+    # out0 = out0.numpy().transpose(1,2,0)
 
-    for j in range(0,len(d),10):
-        imj, labj = d[j]
-        print(labj)
-        plt.imshow(imj.numpy().transpose(1,2,0))
-        plt.show()
-        input("Press enter")
+    # for j in range(0,len(d),10):
+    #     imj, labj = d[j]
+    #     print(labj)
+    #     plt.imshow(imj.numpy().transpose(1,2,0))
+    #     plt.show()
+    #     input("Press enter")
+
+    # for j in range(0,len(vid2),10):
+    #     imj, labj = d[j]
+    #     print(labj)
+    #     plt.imshow(imj.numpy().transpose(1,2,0))
+    #     plt.show()
+    #     input("Enter:")
 
 
 # cv2.CAP_PROP_FRAME_WIDTH
